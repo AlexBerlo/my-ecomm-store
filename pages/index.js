e98905d8f2a743ae82ed072d5d04ec6725ce0bab
AlexBerlo <berlo1989@gmail.com>
2021-02-13 17:19:01 +0200

    Change name

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to My Store !</h1>
 
-        <p className={styles.description}>The best store!</p>
+        <p className={styles.description}>The best store evah!</p>
 
         <ul className={styles.grid}>
           {products.map((product) => {
