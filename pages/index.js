a6ede313165f39c4554d35ec0407725507d48f97
AlexBerlo <berlo1989@gmail.com>
2021-02-13 17:42:46 +0200

    Integrate stripe

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import products from "../products.json";
-
+import {initiateCheckout} from "../lib/payments.js";
 export default function Home() {
-  console.log(products);
+  console.log(
+    "NEXT_PUBLIC_STRIPE_API_KEY",
+    process.env.NEXT_PUBLIC_STRIPE_API_KEY
+  );
   return (
     <div className={styles.container}>
       <Head>
@@ -27,6 +30,23 @@ export default function Home() {
                   <p>{description}</p>
                   <p>${price}</p>
                 </a>
+                <p>
+                  <button
+                    className={styles.button}
+                    onClick={() => {
+                      initiateCheckout({
+                        lineItems: [
+                          {
+                            price: id,
+                            quantity: 1,
+                          },
+                        ],
+                      });
+                    }}
+                  >
+                    Buy now
+                  </button>
+                </p>
               </li>
             );
           })}
